0f0c14e955515242229a1343bab991a66601d809
MirMello <mirandaleeallen@gmail.com>
2022-08-31 14:29:09 -0600

    fixed seeds and pulling routes issues

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
+const path = require('path');
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
-const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const sequelize = require("./config/connection");
 
+
+// Use apiRoutes
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+  app.listen(PORT, () => console.log('Now listening'));
 });
